b147c6eb51885fffbbc85de9bfcf9043cb659072
suyashgpt <85890745+suyashgpt@users.noreply.github.com>
2021-10-02 11:52:56 +0530

    My first program to convert Temperature.

---
+++ celcius.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() 
+{
+    float celcius= 37;
+    float far = (celcius * 9/5) + 32 ;
+
+    printf("the value of this coversion of celsius in farhenite, %f", far);
+    return 0;
+}
